e140a4ed72adebeefdf941ebd5c759b32f4b54b2
aryankhamar20-cloud <aryankhamar20@gmail.com>
2026-06-01 10:13:19 +0530

    fix: relocate ProductDetailClient out of dynamic route, fix .gitignore

--- app/product/page.tsx
+++ app/product/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 import { Suspense } from 'react'
 import { useSearchParams } from 'next/navigation'
-import ProductDetailClient from './[slug]/ProductDetailClient'
+import ProductDetailClient from './ProductDetailClient'
 import Spinner from '@/components/ui/Spinner'
 
 function ProductPageContent() {
